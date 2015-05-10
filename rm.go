68e1d8cd38ac6d5e80e26c21933db92f997d995c
U-bjcheny-dell\bjcheny <companycy@gmail.com>
2015-05-10 12:37:48 +0800

    check existence of file/dir given, and delete recursively when it's a dir

--- rm.go
+++ rm.go
@@ -4,12 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"log"
 )
 
 func main() {
-
 	flag.Parse()
-	err := os.Remove(flag.Arg(0))
+	src, err := os.Stat(flag.Arg(0))
+	if err != nil {		// if existing
+		log.Fatal(err)
+	}
+
+	if src.IsDir() {
+		err = os.RemoveAll(flag.Arg(0)) // default with -r
+	} else {
+		err = os.Remove(flag.Arg(0))
+	}
 
 	if err != nil {
 		fmt.Println(err)
